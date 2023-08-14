9beeb6d9f35e54706be0c01bc5e025f683b271ff
DESKTOP-CGO3JJS\Sahil S <sahil.s@theswipewire.com>
2023-08-14 09:47:39 +0530

    Commit 2023.08.14.a

--- Assets/Scripts/StrikerController.cs
+++ Assets/Scripts/StrikerController.cs
@@ -10,13 +10,15 @@ public class StrikerController : MonoBehaviour
     private Camera MainCam;
     public LayerMask GroundLayer;
 
+    private Vector3 TargetPoint;
+
     private void Start()
     {
         StrikerRB = GetComponent<Rigidbody>();
         MainCam = Camera.main;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         if (MovingStriker)
         {
@@ -25,21 +27,52 @@ public class StrikerController : MonoBehaviour
             {
                 Vector3 targetPoint = hit.point;
                 targetPoint.y = StrikerRB.position.y;
+                //TargetPoint = hit.point;
+                //TargetPoint.y = StrikerRB.position.y;
+                Vector3 dir = (targetPoint - StrikerRB.position).normalized;
+                StrikerRB.MovePosition(StrikerRB.position + MovingSpeed * Time.deltaTime * dir);
+                //StrikerRB.AddForce(MovingSpeed * dir, ForceMode.VelocityChange);
+                //StrikerRB.velocity = MovingSpeed * dir;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (MovingStriker)
+        {
+            /*Ray ray = MainCam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, 100, GroundLayer))
+            {
+                Vector3 targetPoint = hit.point;
+                targetPoint.y = StrikerRB.position.y;
                 Vector3 dir = (targetPoint - StrikerRB.position).normalized;
                 //StrikerRB.MovePosition(StrikerRB.position + MovingSpeed * Time.deltaTime * dir);
                 //StrikerRB.AddForce(MovingSpeed * dir, ForceMode.VelocityChange);
                 StrikerRB.velocity = MovingSpeed * dir;
+            }*/
+            /*if (Vector3.Distance(StrikerRB.position, TargetPoint) > 0.5f)
+            {
+                Vector3 dir = (TargetPoint - StrikerRB.position).normalized;
+                StrikerRB.AddForce(MovingSpeed * dir, ForceMode.VelocityChange);
+                //StrikerRB.velocity = MovingSpeed * dir;
             }
+            else
+            {
+                StrikerRB.velocity = Vector3.zero;
+            }*/
         }
     }
 
     private void OnMouseDown()
     {
+        //StrikerRB.constraints = RigidbodyConstraints.FreezePositionY;
         MovingStriker = true;
     }
 
     private void OnMouseUp()
     {
         MovingStriker = false;
+        StrikerRB.velocity = Vector3.zero;
     }
 }
